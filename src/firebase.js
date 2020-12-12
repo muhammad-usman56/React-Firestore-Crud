3317cb76770bc0f98e18c437139c58fbf6d49c14
Muhammad Usman Shamas <47473401+Muhammad-Usman56@users.noreply.github.com>
2020-12-12 17:10:05 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,6 @@ import  firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/firestore'
 var firebaseConfig = {
-   apiKey: "AIzaSyBNGqdec1pf9kH-i933ClXuJiJ5aYMpjlE",
-    authDomain: "fire-c36da.firebaseapp.com",
-    projectId: "fire-c36da",
-    storageBucket: "fire-c36da.appspot.com",
-    messagingSenderId: "89550162321",
-    appId: "1:89550162321:web:31c5c28bff437a3ad6f230"
+   
  };
  firebase.initializeApp(firebaseConfig);
